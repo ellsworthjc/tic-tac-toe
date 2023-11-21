42b49dfd2e5acc2441adee78e927d5500872cad1
Connor Ellsworth <ellsworthjc@gmail.com>
2023-11-20 22:08:37 -0500

    beginnings of actual minimax

--- js/minimax.js
+++ js/minimax.js
@@ -1,6 +1,40 @@
-export function minimax() {
+import * as logic from "./logic.js";
+
+/**
+ *
+ * @param {Array} board
+ * @param {Array} emptyCells
+ * @param {Number} depth
+ * @param {Boolean} isMaximizing
+ * @returns {Number} value of best move
+ */
+export function minimax(board, depth, isMaximizing) {
 	console.log("...minimaxing...");
-	return 1;
+
+	// START ACTUAL MINIMAX //
+	// const gameEnd = logic.checkForGameEnd(board);
+	// if (gameEnd) {
+	// 	console.log("Value:", gameEnd.value);
+	// 	return gameEnd.value;
+	// }
+
+	// const emptyCells = getEmptyCellIndexes(board);
+
+	// if (isMaximizing) {
+	// 	console.log("maximizing");
+	// 	minimax(board, emptyCells, 0, true);
+	// } else {
+	// 	console.log("minimizing");
+	// 	minimax(board, emptyCells, 0, false);
+	// }
+	// END ACTUAL MINIMAX //
+
+	// just return random cell for now
+	const emptyCells = getEmptyCellIndexes(board);
+	const move = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+
+	console.log({move});
+	return move;
 }
 
 /**
@@ -19,17 +53,15 @@ export function getEmptyCellIndexes(board) {
 }
 
 /**
+ * Essentially a "getBestMove"
  *
  * @param {Array} board
  * @returns {Number} cell number to play best move
  */
 export function takeAITurn(board) {
 	console.log("AI thinking...");
-	const emptyCells = getEmptyCellIndexes(board);
+	let bestMove = -Infinity;
 
-	// return random cell for now but we would minimax here
-	minimax();
-	const move = emptyCells[Math.floor(Math.random() * emptyCells.length)];
-	console.log({move});
-	return move;
+	bestMove = Math.max(bestMove, minimax(board, 0, true));
+	return bestMove;
 }
